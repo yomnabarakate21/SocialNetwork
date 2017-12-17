3e5b03de1664eb32073a2e55f69f52c9fad8de14
yomnabarakate21 <yomnabarakate21@gmail.com>
2017-12-17 19:02:37 +0200

    Add mysql model for dataa=base connection

---
+++ models/mysql.js
@@ -0,0 +1,8 @@
+var mysqlModel = require('mysql-model');
+var MyAppModel = mysqlModel.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'SocialNetwork',
+});
+module.exports= MyAppModel;
