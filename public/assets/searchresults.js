430f20b1e3b8fe1eb30392e26621ebddf0f7eca3
raffy-bekhit <raffybekhit@gmail.com>
2017-12-20 00:19:03 +0200

    Add searchresults

---
+++ public/assets/searchresults.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  $("#submitSearchForm").click(function() {
+    var firstname=$('#firstname');
+    var lastname=$('#lastname');
+    var email=$('#email');
+    var hometown=$('#hometown');
+    var caption=$('#caption');
+    var searchquery={firstname:firstname.val(),lastname:lastname.val(),email:email.val(),hometown:hometown.val(),caption:caption.val()};
+    console.log("I was submitted");
+    $.ajax({
+      type: 'POST',
+      url: '/searchresults',
+      data:searchquery
+    });
+  });
+  });
---
+++ routes/searchresults.js
@@ -0,0 +1,91 @@
+var http = require('http');
+var mysql = require('../models/mysql-connection.js');
+var User = require('../models/user.js');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+var checkNullString = require('check-null-string');
+
+var user = new User();
+
+
+
+module.exports = function(app){
+
+
+
+app.post('/searchresults', urlencodedParser, function(req, res){
+var firstname = req.body.firstname;
+var lastname = req.body.lastname;
+var email = req.body.email;
+var hometown = req.body.hometown;
+var caption = req.body.caption;
+console.log(firstname);
+
+
+
+if(checkNullString(firstname) && checkNullString(lastname) && checkNullString(email) && checkNullString(hometown ) && checkNullString(caption) )
+
+{
+  console.log("hinulllll");
+}
+
+else {
+    var myQuery = "select * from MyUser where "
+    if(checkNullString(firstname)===false)
+    myQuery += " firstname = " + mysql.escape(firstname);
+    if(checkNullString(lastname)===false)
+    myQuery += " AND lastname = " + mysql.escape(lastname);
+    if(checkNullString(email)===false)
+    myQuery += " AND email = " + mysql.escape(email);
+    if(checkNullString(hometown)===false)
+    myQuery += " AND hometown = " + mysql.escape(hometown);
+    if(checkNullString(caption)===false)
+    myQuery += " AND email like " + mysql.escape('%'+caption+'%');
+
+    user.query(myQuery,function(err,rows,fields)
+{
+     if (err){ throw err;
+            console.log(err);
+
+     }
+
+     else { //results
+       for (var i = 0; i < rows.length; i++) {
+         console.log(rows[i]);
+
+
+       }
+
+app.post('/searchresults', urlencodedParser, function(req, res){
+       res.render('searchresults.ejs', {
+         results : rows
+       });
+
+
+     }
+   );
+
+
+ }});
+
+
+
+/*
+app.get('/search', function(req, res, next) {
+  res.send("You are in the profile of the user of id" + req.params.id);
+  next();
+
+
+});*/
+/*app.post('/search', urlencodedParser, function(req, res) {
+
+
+
+});*/
+
+
+}
+});
+}
