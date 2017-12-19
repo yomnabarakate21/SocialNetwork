cddfdcc49186d8c2f45bf5282e9a970425d3ae45
raffy-bekhit <raffybekhit@gmail.com>
2017-12-19 14:45:32 +0200

    merge files

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ var index = require ('./routes/index');
 
 var post= require('./routes/post');
 var user= require('./routes/user');
-<<<<<<< HEAD
-=======
+
 var friendreq= require('./routes/friendreq');
 
->>>>>>> ef9db6b60b2f8e06ea8e92ab0bcf9b52d7472fb6
 //body parser middle ware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -41,11 +39,7 @@ user(app);
 //
 
 
-<<<<<<< HEAD
 var port = 4001;
-=======
-var port = 5000;
->>>>>>> ef9db6b60b2f8e06ea8e92ab0bcf9b52d7472fb6
 //listen to the specified port
 app.listen(port, function(){
   console.log('Server started on port '+port);
--- routes/user.js
+++ routes/user.js
@@ -1,46 +1,13 @@
-<<<<<<< HEAD
-var mysql = require('../models/mysql-connection.js');
-var User = require('../models/user.js');
-=======
 //use normal connection here
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "SocialNetwork"
-});
-
-
-
+var con = require('../models/mysql-connection');
 
 
 //require the user model for using it.
 const User = require('../models/user');
->>>>>>> ef9db6b60b2f8e06ea8e92ab0bcf9b52d7472fb6
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
-<<<<<<< HEAD
-var checkNullString = require('check-null-string');
-var json = require('json');
-var user = new User();
-module.exports = function(app){
-app.post('/search', urlencodedParser,function(req,res)
-{
-
-
-var firstname = req.body.firstname;
-var lastname = req.body.lastname;
-var email = req.body.email;
-var hometown = req.body.hometown;
-var caption = req.body.caption;
-console.log(email);
-
-
-=======
 var data=[];
 module.exports = function(app) {
     app.get('/user/home/:id', function(req, res, next) {
@@ -80,49 +47,6 @@ module.exports = function(app) {
 
 
       });
->>>>>>> ef9db6b60b2f8e06ea8e92ab0bcf9b52d7472fb6
 
-if(checkNullString(firstname) && checkNullString(lastname) && checkNullString(email) && checkNullString(hometown ) && checkNullString(caption) )
-{
 
-<<<<<<< HEAD
 }
-
-else {
-    var myQuery = "select * from MyUser where "
-    if(checkNullString(firstname)===false)
-    myQuery += " firstname = " + mysql.escape(firstname);
-    if(checkNullString(lastname)===false)
-    myQuery += " AND lastname = " + mysql.escape(lastname);
-    if(checkNullString(email)===false)
-    myQuery += " AND email = " + mysql.escape(email);
-    if(checkNullString(hometown)===false)
-    myQuery += " AND hometown = " + mysql.escape(hometown);
-    if(checkNullString(caption)===false)
-    myQuery += " AND email like " + mysql.escape('%'+caption+'%');
-
-    console.log(myQuery);
-
-    user.query(myQuery,function(err,result)
-  {
-     if (err){ throw err;
-            console.log(err);
-
-     }
-
-     else { //results
-console.log(result);
-
-     }
-
-  }
-);
-}
-});
-app.get('/search', urlencodedParser, function(req,res){
-  res.render('search');
-});
-}
-=======
-    }
->>>>>>> ef9db6b60b2f8e06ea8e92ab0bcf9b52d7472fb6
