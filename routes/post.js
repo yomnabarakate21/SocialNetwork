3a0069c196fa9cf86875b304331fe0452cff4b06
ZeyadZanaty <zeyadzanaty@Zeyads-MacBook-Pro.local>
2017-12-23 00:55:29 +0200

    added images in post

--- routes/post.js
+++ routes/post.js
@@ -5,19 +5,53 @@ var urlencodedParser = bodyParser.urlencoded({
 });
 
 module.exports = function(app) {
-  
+
 
   //Add a new post to the post Table
   app.post('/post', urlencodedParser, function(req, res) {
+    console.log("Posting");
+    var post=req.body;
+    var pcaption=post.caption;
+    var id= post.id_label;
+    var pstatus=post.privacy;
+    console.log(post);
+    if(req.files.postpic)
+    {
+      var file = req.files.postpic;
+      var img_name = file.name;
 
+      if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+
+        file.mv('public/images/post_images/' + file.name, function(err) {
+
+            if (err)
+            return res.status(500).send(err);
+            mypost=new Posts({
+              caption:pcaption,
+              poster_id: id,
+              ispublic: pstatus,
+              image:img_name,
+            });
+            mypost.save();
+          });
+        }
+        else {
+          message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+
+          res.render('temp.ejs', {
+            message: message
+          });
+        }
+    }
+    else {
     mypost=new Posts({
-      caption:req.body.caption,
-      poster_id: req.body.poster_id,
-      ispublic: req.body.ispublic,
+      caption:pcaption,
+      poster_id: id,
+      ispublic: pstatus,
     });
     mypost.save();
-    res.send(mypost);
-
+  }
+    res.redirect("/user/home/"+id);
   });
 
 
