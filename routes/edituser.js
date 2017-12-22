b1ad2552aa94c6a3f25e6ebb4a149d5a80d5dd65
yomnabarakate21 <yomnabarakate21@gmail.com>
2017-12-22 17:21:42 +0200

    Fix EditProfile

--- routes/edituser.js
+++ routes/edituser.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var md5 = require('md5');
-//var io =require("socket.io")(http);
+var checkNullString = require('check-null-string');
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,76 +19,113 @@ var urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
 module.exports = function(app) {
-  app.get('/editprofile/:id', function(req, res, next) {
-    id = req.params.id;
-    con.query("SELECT * FROM MyUser WHERE MyUser.user_id=?", req.params.id, function(err, rows, fields) {
+    app.get('/editprofile/:id', function(req, res, next) {
+      id = req.params.id;
+      con.query("SELECT * FROM MyUser WHERE MyUser.user_id=?", req.params.id, function(err, rows, fields) {
 
-      x = rows[0].password;
-
-
-
-      if (rows != "") {
-
-
-        res.render('edituser.ejs', {
-          data: rows
-        });
-      }
-
-
-    });
-  });
+        x = rows[0].password;
 
 
 
-
-  app.post('/user/editprofile/:id', function(req, res, next) {
-    if (req.files.image) {
-      console.log('yarab');
-    }
-    if (req.body.lastname != "") {
-      console.log('uadating lastname: ' + req.body.lastname);
-      con.query("UPDATE MyUser SET lastname=? WHERE MyUser.user_id=?", [req.body.lastname, id]);
-
-      sleep(1000);
-    }
+        if (rows != "") {
 
 
-    if (req.body.firstname != "") {
-      con.query("UPDATE MyUser SET firstname=? WHERE MyUser.user_id=?", [req.body.firstname, id]);
-      sleep(1000);
-    }
+          res.render('temp.ejs', {
+            data: rows
+          });
+        }
 
-    if (req.body.phone_number1 != "") {
-      console.log('uadating: ' + req.body.lastname);
-      con.query("UPDATE MyUser SET phone_number1=? WHERE MyUser.user_id=?", [req.body.lastname, id]);
 
-      sleep(1000);
-    }
-
-    if (req.body.hometown != "") {
-      console.log('uadating: ' + req.body.hometown);
-      con.query("UPDATE MyUser SET hometown=? WHERE MyUser.user_id=?", [req.body.hometown, id]);
-
-      sleep(1000);
-    }
-
-    if (req.body.password_1 != "" && req.body.password != "") {
-      var pass = md5(req.body.password_1);
-      
-      if (pass == x) {
-        console.log('uadating password: ' + req.body.password_1);
-
-        con.query("UPDATE MyUser SET password=? WHERE MyUser.user_id=?", [pass, id]);
-      } else {
-        res.status(500).send('showAlert');
-
-      }
-      sleep(1000);
-    }
+      });
+    }); //end of get function
 
 
 
-  });
 
-};
+
+    app.post('/editprofile/:id', function(req, res, next) {
+         var id = req.params.id;
+          var post = req.body;
+          var name = post.euser_name;
+          var pass = post.epassword;
+          var fname = post.efirst_name;
+          var lname = post.elast_name;
+          var mob = post.emob_no;
+          var hometown = post.ehome_town;
+          var status = post.emarital_status;
+          if (req.files.euploaded_image) {
+            var file = req.files.euploaded_image;
+            var img_name = file.name;
+
+            if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+
+              file.mv('public/images/upload_images/' + file.name, function(err) {
+
+                  if (err)
+                  return res.status(500).send(err);
+
+                  var query = con.query("UPDATE MyUser SET  MyUser.profile_picture=? WHERE MyUser.user_id=?", [img_name, req.params.id], function(err, result) {
+                    if (err)
+                    throw err;
+
+                  });
+                });
+
+              }
+              else {
+                message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+                console.log('this format isnt allowed');
+                res.render('temp.ejs', {
+                  message: message
+                });
+              }
+
+            }
+
+            if (!checkNullString(req.body.elast_name)) {
+              console.log('upadating lastname: ' + req.body.elast_name);
+              con.query("UPDATE MyUser SET lastname=? WHERE MyUser.user_id=?", [req.body.elast_name, id]);
+
+              sleep(1000);
+            }
+
+
+            if (!checkNullString(req.body.efirst_name)) {
+                console.log('upadating firstname: ' + req.body.efirst_name);
+              con.query("UPDATE MyUser SET firstname=? WHERE MyUser.user_id=?", [req.body.efirst_name, id],function(err, result) {
+
+              });
+              sleep(1000);
+            }
+
+            if (!checkNullString(mob)) {
+              console.log('upadating mob: ' + req.body.emob_no);
+              con.query("UPDATE MyUser SET phone_number1=? WHERE MyUser.user_id=?", [req.body.emob_no, id]);
+              sleep(1000);
+            }
+
+            if (!checkNullString(req.body.ehome_town)) {
+              console.log('uadating: ' + req.body.hometown);
+              con.query("UPDATE MyUser SET hometown=? WHERE MyUser.user_id=?", [req.body.ehome_town, id]);
+
+              sleep(1000);
+            }
+
+            if (!checkNullString(req.body.epassword_1) && !checkNullString(req.body.epassword)) {
+              var pass = md5(req.body.epassword_1);
+
+              if (pass == x) {
+                console.log('upadating password: ' + req.body.epassword_1);
+
+                con.query("UPDATE MyUser SET password=? WHERE MyUser.user_id=?", [pass, id]);
+              } else {
+                   console.log('Passwords')
+
+              }
+              sleep(1000);
+            }
+
+          res.render('edituser.ejs',{data:id});
+
+          });
+        }
--- routes/index.js
+++ routes/index.js
@@ -1,94 +1,70 @@
-
-
-/*module.exports = function(app){
-  app.get('/',function(req,res,next){
-    res.render('index.html');
-  });
-}
-*/
 var db = require('../models/mysql-connection.js');
-module.exports = function(app){
-    message = '';
-   app.post('/',function(req,res,next){
-      var post  = req.body;
-      var name= post.user_name;
-      var pass= post.password;
-      var fname= post.first_name;
-      var lname= post.last_name;
-      var mob= post.mob_no;
-      var gender=post.gender;
-      var hometown = post.home_town;
-      var email = post.email;
-      var status = post.marital_status;
-      var q = "SELECT * FROM MyUser WHERE email = ?";
-      var query = db.query(q,email,function(err,result){
-        if(result.length>0)
-        {
-          return res.status(200).send('Email taken');
-        }
-      });
-	  if (!req.files.uploaded_image)
-    {
-      console.log("hena");
-				//return res.status(400).send('No files were uploaded.');
+module.exports = function(app) {
+  message = '';
+  app.post('/', function(req, res, next) {
+    var post = req.body;
+    var name = post.user_name;
+    var pass = post.password;
+    var fname = post.first_name;
+    var lname = post.last_name;
+    var mob = post.mob_no;
+    var gender = post.gender;
+    var hometown = post.home_town;
+    var email = post.email;
+    var status = post.marital_status;
+    var q = "SELECT * FROM MyUser WHERE email = ?";
+    var query = db.query(q, email, function(err, result) {
+      if (result.length > 0) {
+        return res.status(200).send('Email taken');
+      }
+    });
+    if (!req.files.uploaded_image) {
+      //return res.status(400).send('No files were uploaded.');
 
-          //  return res.status(500).send(err);
-          if(gender=="male")
-          var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','m_default.jpg','"+gender+"','"+email+"','"+hometown+"','"+status+"')";
-          else
-          var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','f_default.jpg','"+gender+"','"+email+"','"+hometown+"','"+status+"')";
+      //  return res.status(500).send(err);
+      if (gender == "male")
+        var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','m_default.jpg','" + gender + "','" + email + "','" + hometown + "','" + status + "')";
+      else
+        var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','f_default.jpg','" + gender + "','" + email + "','" + hometown + "','" + status + "')";
+
+      var query = db.query(sql, function(err, result) {
+        console.log('am here! and near');
+        res.redirect('/user/home/' + result.insertId);
+      });
+
+    } else {
+      var file = req.files.uploaded_image;
+      var img_name = file.name;
+
+      if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+
+        file.mv('public/images/upload_images/' + file.name, function(err) {
+
+          if (err)
+
+            return res.status(500).send(err);
+          var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','" + img_name + "','" + gender + "','" + email + "','" + hometown + "','" + status + "')";
 
           var query = db.query(sql, function(err, result) {
-            console.log('am here! and near');
-             res.redirect('/user/home/'+result.insertId);
+
+            res.redirect('/user/home/' + result.insertId);
+
           });
-
+        });
+      } else {
+        message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+        console.log('this format isnt allowed');
+        res.render('index.ejs', {
+          message: message
+        });
       }
-    else{
-		var file = req.files.uploaded_image;
-		var img_name=file.name;
-
-	  	 if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
-
-              file.mv('public/images/upload_images/'+file.name, function(err) {
-
-	              if (err)
-
-	                return res.status(500).send(err);
-                  var sql = "INSERT INTO `MyUser`(`firstname`,`lastname`,`phone_number1`,`nickname`, `password` ,`profile_picture`,`gender`,`email`,`hometown`,`marital_status`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','"+img_name+"','"+gender+"','"+email+"','"+hometown+"','"+status+"')";
-
-    						var query = db.query(sql, function(err, result) {
-                  console.log('am here! and near');
-
-    							 res.redirect('/user/home/'+result.insertId);
-
-    						});
-					   });
-          } else {
-          //  message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
-
-console.log('this format isnt allowed');
-
-            //res.render('index.ejs',{message: message});
-          }
-        }
-   });
+    }
+  });
 
 
-   app.get('/',function(req,res,next){
-     res.render('index.html');
-   });
+  app.get('/', function(req, res, next) {
+    res.render('index.html');
+  });
 
-/*exports.profile = function(req, res){
-	var message = '';
-	var id = req.params.id;
-    var sql="SELECT * FROM `users_image` WHERE `id`='"+id+"'";
-    db.query(sql, function(err, result){
-	  if(result.length <= 0)
-	  message = "Profile not found!";
 
-      res.render('profile.ejs',{data:result, message: message});
-   });
-};
-*/
 }
