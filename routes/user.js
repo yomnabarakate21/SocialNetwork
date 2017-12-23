8a6bf00b75858f97aea147d7c9acda98aac3d7c8
yomnabarakate21 <yomnabarakate21@gmail.com>
2017-12-23 23:14:37 +0200

    FIX routes in search

--- routes/user.js
+++ routes/user.js
@@ -44,7 +44,7 @@ module.exports = function(app) {
   app.get('/user/home/:id', function(req, res, next) {
     var id = req.params.id;
     //get all the friends
-    con.query(" SELECT user_id FROM MyUser JOIN (SELECT * FROM Friendship WHERE ((Friendship.user_id1 =? OR Friendship.user_id2 =? )AND Friendship.status='0'))as t1  ON ((MyUser.user_id= t1.user_id1 OR MyUser.user_id= t1.user_id2) AND MyUser.user_id<>?) ", [req.params.id, req.params.id, req.params.id],
+    con.query(" SELECT user_id FROM MyUser JOIN (SELECT * FROM Friendship WHERE ((Friendship.user_id1 =? OR Friendship.user_id2 =? )AND Friendship.status='1'))as t1  ON ((MyUser.user_id= t1.user_id1 OR MyUser.user_id= t1.user_id2) AND MyUser.user_id<>?) ", [req.params.id, req.params.id, req.params.id],
       function(err, rows, fields) {
         if (err) throw err;
         setids(rows, function() {
